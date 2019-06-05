9055c7c14f99b521938b2421a75de195ece71616
Michele Brito Pinheiro <mibrito@dcc.ufmg.br>
2019-06-05 17:50:46 -0300

    start including division

--- TPParadigmas/p2796.py
+++ TPParadigmas/p2796.py
@@ -1,51 +1,64 @@
-EMPTY = '.'
-FILLED = '#'
+def divide (map, r0, r1, c0, c1):
+    cm = ((c1 - c0) / 2)
+    rm = ((r1 - r0) / 2)
 
-def quad(map, r0, r1, c0, c1):
-    rm = (r1 - r0) / 2
-    cm = (c1 - c0) / 2
-    # if r1 is r0 and c0 is c1:
-    #     return (1, 1) if map[r0][c0] is EMPTY else (0, 0)
-    # elif r1 is not r0 and c0 is c1:
-    #     a = quad(map, r0, r0 + rm, c0, c1)
-    #     b = quad(map, r0 + rm + 1, r1, c0, c1)
-    #     return (max(a[0], b[0]) - min(a[0], b[0]), r0)
-    # elif r1 is r0 and c0 is not c1:
-    #     a = quad(map, r0, r1, c0, c0 + cm)
-    #     b = quad(map, r0, r1, c0 + cm + 1, c1)
-    #     return (r0, max(a[1], b[1]) - min(a[1], b[1]))
+    res = []
 
-    # if (r1 -  < )
-    print(rm, cm)
-    print('a', r0, r0 + rm, c0, c0 + cm)
-    print('b', r0, r0 + rm, c0 + cm + 1, c1)
+    # print "\t".join([str(r0), str(r1), str(c0), str(c1), str(rm), str(cm)])
 
-    print('c', r0 + rm + 1, r1, c0, c0 + cm)
-    print('d', r0 + rm + 1, r1, c0 + cm + 1, c1)
+    if rm < 0 or cm < 0:
+        return None
 
-    # a = quad(map, r0, r0 + rm, c0, c0 + cm)
-    # b = quad(map, r0, r0 + rm, c0 + cm + 1, c1)
-    # c = quad(map, r0 + rm + 1, r1, c0, c0 + cm)
-    # d = quad(map, r0 + rm + 1, r1, c0 + cm + 1, c1)
-    # return (max(a[0], b[0]) - min(a[0], b[0]), r0, max(a[1], b[1]) - min(a[1], b[1]))
+    if (r1 <= r0 and c1 <= c0):
+        return ((r0, r1, c0, c1), '.') if map[r0][c0] is '.' else None
 
-def main():
-    entry = raw_input().split()
-    N = int(entry[0])
-    M = int(entry[1])
+    if r1 <= r0:
+        a = divide(map, r0, r1, c0, c0 + cm)
+        b = divide(map, r0, r1, c0 + cm + 1, c1)
+        if a is b and a is None:
+            return None
+        else:
+            return ((r0, r1, c0, c1), [a, b, None, None])
+
+    if c1 <= c0:
+        a = divide(map, r0, r0 + rm, c0, c1)
+        c = divide(map, r0 + rm + 1, r1, c0, c1)
+        if a is b and a is None:
+            return None
+        else:
+            return ((r0, r1, c0, c1), [a, None, c, None])
+
+    res = (
+        (r0, r1, c0, c1),
+        [
+            divide(map, r0, r0 + rm, c0, c0 + cm),
+            divide(map, r0, r0 + rm, c0 + cm + 1, c1),
+            divide(map, r0 + rm + 1, r1, c0, c0 + cm),
+            divide(map, r0 + cm + 1, r1, c0 + cm + 1, c1)
+        ]
+    )
+
+    return res
+
+def  main ():
+    linha = raw_input().split()
+    N =  int (linha[ 0 ])
+    M =  int (linha[ 1 ])
 
     map = []
     for i in range(N):
-        map.append(raw_input())
+        line = raw_input()
+        map.append(line)
 
-    K = int(raw_input())
     tables = []
+    K = int(raw_input())
     for i in range(K):
-        entry = raw_input().split()
-        tables.append((int(entry[0]), int(entry[1])))
+        line = raw_input().split()
+        tables.append((int(line[0]), int(line[1])))
 
-    quad(map, 0, N-1, 0, M - 1)
+    print divide(map, 0, N-1, 0, M-1)
 
 
 if __name__ == '__main__':
     main()
+
